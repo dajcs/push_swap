509a4e9e339a2261b9286f380acf06c1d0941c77
Attila Nemet <anemet@c2r7s5.42luxembourg.lu>
2025-07-10 14:27:03 +0200

    header email

--- 03_operations_swap.c
+++ 03_operations_swap.c
@@ -1,33 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*   03_operations_swap.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: anemet <anemet@student.42luxembourg.lu>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/06/13 14:17:09 by anemet            #+#    #+#             */
-/*   Updated: 2025/07/10 14:25:59 by anemet           ###   ########.fr       */
+/*   Created: 2025/07/10 14:23:30 by anemet            #+#    #+#             */
+/*   Updated: 2025/07/10 14:23:31 by anemet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
-
-void	ft_lstclear(t_list **lst, void (*del)(void *))
-{
-	t_list	*cur;
-	t_list	*next;
-
-	cur = NULL;
-	next = NULL;
-	if (!lst || !del)
-		return ;
-	cur = *lst;
-	while (cur)
-	{
-		next = cur->next;
-		del(cur->content);
-		free(cur);
-		cur = next;
-	}
-	*lst = NULL;
-}
